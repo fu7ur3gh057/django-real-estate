e0638f881230b7ebd7c37b0b27591882c37285f0
fu7ur3gh057 <fu7ur3gh057@gmail.com>
2022-08-31 14:53:57 +0400

    add ratings and enquiries views

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -4,13 +4,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('super-admin/', admin.site.urls),
-                  path('api/v1/auth/', include("djoser.urls")),
-                  path('api/v1/auth/', include("djoser.urls.jwt")),
-                  path('api/v1/profile/', include("apps.profiles.urls")),
-                  path('api/v1/properties/', include("apps.properties.urls")),
-
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('super-admin/', admin.site.urls),
+    path('api/v1/auth/', include("djoser.urls")),
+    path('api/v1/auth/', include("djoser.urls.jwt")),
+    path('api/v1/profile/', include("apps.profiles.urls")),
+    path('api/v1/properties/', include("apps.properties.urls")),
+    path('api/v1/ratings/', include("apps.ratings.urls")),
+    path('api/v1/enquiries/', include("apps.enquiries.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = "Real Estate Admin Portal"
